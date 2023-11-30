1fa6fa95b651035e9e52e948902b767511f6a205
Simon-A-Victor <149576144+Simon-A-Victor@users.noreply.github.com>
2023-11-30 22:20:15 +0100

    Add files via upload

---
+++ DrawPanel.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+// This panel represent the animated part of the view with the car images.
+
+public class DrawPanel extends JPanel{
+
+
+    private ArrayList<BufferedImage> images;
+
+    private ArrayList<Point> points;
+
+    void moveit(int x, int y, int index){
+        points.get(index).x = x;
+        points.get(index).y = y;
+    }
+
+    // Initializes the panel and reads the images
+    public DrawPanel(int x, int y, ArrayList<MotorVehicle> cars) {
+        this.setDoubleBuffered(true);
+        this.setPreferredSize(new Dimension(x, y));
+        this.setBackground(Color.green);
+        this.images = new ArrayList<BufferedImage>();
+        this.points = new ArrayList<Point>();
+        // Print an error message in case file is not found with a try/catch block
+        try {
+            for (MotorVehicle car : cars){
+                points.add(new Point());
+                images.add(ImageIO.read(DrawPanel.class.getResourceAsStream("pics/"+car.getModelName()+".jpg")));
+            }
+        } catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    // This method is called each time the panel updates/refreshes/repaints itself
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for(int index = 0; index < images.size(); index++){
+            g.drawImage(images.get(index), points.get(index).x, points.get(index).y, null); // see javadoc for more info on the parameters
+        }
+    }
+}
